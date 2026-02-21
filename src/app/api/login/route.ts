8cbaa9ddaa4a1771149a6ea98b56cdeeea49da82
Alexandre Godin <alex.godin@pm.me>
2026-02-21 13:47:05 -0800

    test logs

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,6 +4,10 @@ export async function POST(request: Request) {
   const PASSWORD = process.env.SITE_PASSWORD || 'changeme'
   const { password } = await request.json()
 
+  console.log('PASSWORD ENV:', process.env.SITE_PASSWORD)
+  console.log('PASSWORD RECU:', password)
+  console.log('MATCH:', password === PASSWORD)
+
   if (password !== PASSWORD) {
     return NextResponse.json({ error: 'Mauvais mot de passe' }, { status: 401 })
   }
